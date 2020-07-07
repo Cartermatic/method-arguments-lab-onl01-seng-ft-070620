b3a2af0f09f693032edbe0bb307f0e6475fb3245
Carter Parrett <cparrett94@hotmail.com>
2020-07-07 01:04:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-#introduction_with_language("Dan", "Ember.js")
+def #introduction_with_language("Dan", "Ember.js")
   puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
 end
